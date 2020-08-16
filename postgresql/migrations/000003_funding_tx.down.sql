a6c770462da53169a11633e53b54fe029f9f7278
Yaacov Akiba Slama <ya@slamail.org>
2020-08-16 12:15:02 +0300

    Add postgresql migration files

---
+++ postgresql/migrations/000003_funding_tx.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.payments DROP COLUMN funding_tx_id;
+ALTER TABLE public.payments DROP COLUMN funding_tx_outnum;
---
+++ postgresql/migrations/000003_funding_tx.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.payments ADD funding_tx_id bytea NULL;
+ALTER TABLE public.payments ADD funding_tx_outnum int NULL;
