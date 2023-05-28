59f01bd30c040b3ba61a3c9990e69d52d5d601a8
Yaacov Akiba Slama <ya@slamail.org>
2023-05-28 13:23:34 +0300

    Add data in new_channel_params in the migration

--- postgresql/migrations/000011_new_channel_params.up.sql
+++ postgresql/migrations/000011_new_channel_params.up.sql
@@ -3,3 +3,9 @@ CREATE TABLE public.new_channel_params (
 	params jsonb NOT NULL
 );
 CREATE UNIQUE INDEX new_channel_params_validity_idx ON public.new_channel_params (validity);
+
+INSERT INTO public.new_channel_params (validity, params)
+ VALUES(259200, '{"min_msat": "12000000", "proportional": 7500, "max_idle_time": 4320, "max_client_to_self_delay": 432}'::jsonb);
+
+INSERT INTO public.new_channel_params (validity, params)
+ VALUES(3600, '{"min_msat": "10000000", "proportional": 7500, "max_idle_time": 4320, "max_client_to_self_delay": 432}'::jsonb);
