0f056274c1189b4214235e592d3500b3f107c045
Jesse de Wit <witdejesse@hotmail.com>
2023-10-16 15:43:21 +0200

    only set minconf =0 on zero conf channels

--- itest/cln_breez_client.go
+++ itest/cln_breez_client.go
@@ -39,6 +39,9 @@ def on_openchannel(openchannel, plugin, **kwargs):
 	mindepth = int(0)
 
 	plugin.log(f"This peer is in the zeroconf allowlist, setting mindepth={mindepth}")
+	if openchannel.funding_msat == 200000000:
+	    return {'result': 'continue'}
+
 	return {'result': 'continue', 'mindepth': mindepth}
 
 plugin.run()
