3e392fb792e2192327b1cc02d2c6b35bd2f6e237
Jesse de Wit <witdejesse@hotmail.com>
2024-04-05 20:03:46 +0200

    postgres: log applied migrations

--- postgresql/migrate.go
+++ postgresql/migrate.go
@@ -3,6 +3,7 @@ package postgresql
 import (
 	"embed"
 	"fmt"
+	"log"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
@@ -21,10 +22,28 @@ func Migrate(databaseUrl string) error {
 	if err != nil {
 		return fmt.Errorf("could not connect to db for migrations: %w", err)
 	}
+	m.Log = &MigrationLogger{}
+	version, dirty, err := m.Version()
+	if err != nil && err != migrate.ErrNilVersion {
+		return fmt.Errorf("could not get current database migration version: %w", err)
+	}
+	log.Printf("Current database version is %d, dirty = %t", version, dirty)
 	err = m.Up()
 	if err != nil {
 		return fmt.Errorf("failed to apply database migrations: %w", err)
 	}
 
 	return nil
+}
+
+type MigrationLogger struct {
+}
+
+func (l *MigrationLogger) Printf(format string, v ...interface{}) {
+	log.Printf("Applied migration "+format, v...)
+}
+
+// Verbose should return true when verbose logging output is wanted
+func (l *MigrationLogger) Verbose() bool {
+	return false
 }
