77f8798b7385e209263096717c0fb891df8488ce
Jesse de Wit <witdejesse@hotmail.com>
2024-05-24 15:02:03 +0200

    Add stop context to main function

--- cmd/lspd/main.go
+++ cmd/lspd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/breez/lspd"
 	"github.com/breez/lspd/build"
@@ -15,13 +18,22 @@ func main() {
 	app.Name = "lspd"
 	app.Version = build.GetTag() + " commit=" + build.GetRevision()
 	app.Usage = "LSP implementation for LND and CLN"
-	app.Action = func(ctx *cli.Context) error {
+	app.Action = func(cliCtx *cli.Context) error {
+		ctx, cancel := context.WithCancel(context.Background())
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		go func() {
+			sig := <-c
+			log.Printf("Received stop signal %v. Stopping.", sig)
+			cancel()
+		}()
+
 		config, err := config.LoadConfig()
 		if err != nil {
 			return err
 		}
 
-		return lspd.Main(config)
+		return lspd.Main(ctx, config)
 	}
 	app.Commands = []cli.Command{
 		genKeyCommand,
