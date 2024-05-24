ee71993b1c5f174ce1a6349d05d86479b9a5e810
Jesse de Wit <witdejesse@hotmail.com>
2024-05-24 14:31:24 +0200

    Allow omitting email notifications

--- config/email.go
+++ config/email.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -11,15 +12,34 @@ type Email struct {
 	From string
 }
 
-func addresses(a string) (addr []*string) {
-	json.Unmarshal([]byte(a), &addr)
-	return
+func addresses(a string) ([]*string, error) {
+	var addr []*string
+	err := json.Unmarshal([]byte(a), &addr)
+	return addr, err
 }
 
-func loadOpenChannelEmailSettings() *Email {
-	return &Email{
-		To:   addresses(os.Getenv("OPENCHANNEL_NOTIFICATION_TO")),
-		Cc:   addresses(os.Getenv("OPENCHANNEL_NOTIFICATION_CC")),
-		From: os.Getenv("OPENCHANNEL_NOTIFICATION_FROM"),
+func loadOpenChannelEmailSettings() (*Email, error) {
+	from := os.Getenv("OPENCHANNEL_NOTIFICATION_FROM")
+	to := os.Getenv("OPENCHANNEL_NOTIFICATION_TO")
+	cc := os.Getenv("OPENCHANNEL_NOTIFICATION_CC")
+
+	if from == "" && to == "" && cc == "" {
+		return nil, nil
 	}
+
+	t, err := addresses(to)
+	if err != nil {
+		return nil, fmt.Errorf("invalid OPENCHANNEL_NOTIFICATION_TO: %v", err)
+	}
+
+	c, err := addresses(cc)
+	if err != nil {
+		return nil, fmt.Errorf("invalid OPENCHANNEL_NOTIFICATION_CC: %v", err)
+	}
+
+	return &Email{
+		To:   t,
+		Cc:   c,
+		From: from,
+	}, nil
 }
